b4b8f87a4f0be6f0aad77fdc7e775519c10ef234
andybyers21 <andy.byers21@gmail.com>
2020-09-23 16:12:02 +0100

    update templates

--- src/django_blog_platform/views.py
+++ src/django_blog_platform/views.py
@@ -1,14 +1,16 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'hello_world.html', {"title": "django-blog-platform"})
+    username = request.user.username
+    hometitle = f"Welcome to my django-blog-platform " + username
+    return render(request, 'home.html', {"title": hometitle})
 
 
 def about_page(request):
-    return render(request, 'hello_world.html', {"title": "About Me"})
+    return render(request, 'about.html', {"title": "About Me"})
 
 
 def contact_page(request):
-    return render(request, 'hello_world.html', {"title": "Contact Me"})
+    return render(request, 'contact.html', {"title": "Contact Me"})

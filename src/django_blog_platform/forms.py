70a5ca55ba01bfc465344c18b2b00e0c7b76e27f
andybyers21 <andy.byers21@gmail.com>
2020-09-25 09:48:05 +0100

    validate form data

--- src/django_blog_platform/forms.py
+++ src/django_blog_platform/forms.py
@@ -6,3 +6,10 @@ class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        print(email)
+        if email.endswith("aaa"):
+            raise forms.ValidationError("Email address is not valid, aaa")
+        return email
